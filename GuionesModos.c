75f0cda28774eb361768d00f75392531f088d357
jdelvalle773 <ja.delvalle.2023@alumnos.urjc.es>
2025-12-17 18:51:52 +0100

    Create GuionesModos.c

---
+++ GuionesModos.c
@@ -0,0 +1,23 @@
+// USART PUESTOS
+const char Menu[] = "- Jugador 1, dirijase al puesto azul.\r\n\n"
+                    "- Jugador 2, dirijase al puesto verde.\r\n\n";
+// RONDAS ELEGIDAS
+const char Menu[] = "Rondas elegidas: \r\n\n"
+                    
+// Modo 1
+const char Menu[] = "**** ESTIMULO VISUAL ****\r\n\n" 
+                    "En este modo de juego, deberá fijarse atentamente en la luz roja\r\n"
+                    "del centro del tablero.\r\n\n"
+                    "~~ SOLO EL MÁS RÁPIDO GANARÁ ~~\r\n\n";
+
+// Modo 2
+const char Menu[] = "**** ESTIMULO AUDITIVO ****\r\n\n" 
+                    "En este modo de juego, tendrás que afinar el oído para ganar.\r\n"
+                    "Lávate bien los oidos.\r\n\n"
+                    "~~ SOLO EL MÁS RÁPIDO GANARÁ ~~\r\n\n";
+
+// Modo 3
+const char Menu[] = "**** ESTIMULO AUDITIVO ****\r\n\n" 
+                    "En este modo de juego, tendrás que afinar el oído para ganar.\r\n"
+                    "Lávate bien los oidos.\r\n\n"
+                    "~~ SOLO EL MÁS RÁPIDO GANARÁ ~~\r\n\n";
