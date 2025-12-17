33b3bd265d581e713881ab497eb650f54c652a10
jdelvalle773 <ja.delvalle.2023@alumnos.urjc.es>
2025-12-17 19:23:34 +0100

    Update GuionesModos.c

--- GuionesModos.c
+++ GuionesModos.c
@@ -6,18 +6,18 @@ const char Menu[] = "Rondas elegidas: \r\n\n"
                     
 // Modo 1
 const char Menu[] = "**** ESTIMULO VISUAL ****\r\n\n" 
-                    "En este modo de juego, deberá fijarse atentamente en la luz roja\r\n"
+                    "En este modo de juego, debera fijarse atentamente en la luz roja\r\n"
                     "del centro del tablero.\r\n\n"
-                    "~~ SOLO EL MÁS RÁPIDO GANARÁ ~~\r\n\n";
+                    "~~ SOLO EL MAS RAPIDO GANARA ~~\r\n\n";
 
 // Modo 2
 const char Menu[] = "**** ESTIMULO AUDITIVO ****\r\n\n" 
-                    "En este modo de juego, tendrás que afinar el oído para ganar.\r\n"
-                    "Lávate bien los oidos.\r\n\n"
-                    "~~ SOLO EL MÁS RÁPIDO GANARÁ ~~\r\n\n";
+                    "En este modo de juego, tendras que afinar el oido para ganar.\r\n"
+                    "Lavate bien los oidos.\r\n\n"
+                    "~~ SOLO EL MAS RAPIDO GANARA ~~\r\n\n";
 
 // Modo 3
-const char Menu[] = "**** ESTIMULO AUDITIVO ****\r\n\n" 
-                    "En este modo de juego, tendrás que afinar el oído para ganar.\r\n"
-                    "Lávate bien los oidos.\r\n\n"
-                    "~~ SOLO EL MÁS RÁPIDO GANARÁ ~~\r\n\n";
+const char Menu[] = "**** NOCION DE TIEMPO ****\r\n\n" 
+                    "En este modo de juego, tienes que ser un reloj suizo.\r\n"
+                    "Concentracion total.\r\n\n"
+                    "~~ SOLO EL MAS PRECISO GANARA ~~\r\n\n";
